1abbc67640e6ef576e8ba2b588678cab95ef104b
Tim Patullock - Laptop <oddalicious@gmail.com>
2019-01-25 22:22:08 +1100

    killed everything

--- Source/MelbGGJ2019/MelbGGJ2019.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "MelbGGJ2019.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MelbGGJ2019, "MelbGGJ2019" );
